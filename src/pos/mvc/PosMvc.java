9e18f48421825c2d26527cd90bc90b13a3e55025
BuddikaIsuranga <buddikaisuranga3@gmail.com>
2023-08-10 20:50:04 +0530

    Orders Save With Transaction

--- src/pos/mvc/PosMvc.java
+++ src/pos/mvc/PosMvc.java
@@ -6,6 +6,7 @@ package pos.mvc;
 
 
 import pos.mvc.view.CustomerView;
+import pos.mvc.view.OrderView;
 import pos.mvc.view.itemView;
 
 /**
@@ -18,8 +19,9 @@ public class PosMvc {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //new CustomerView().setVisible(true);
-        new itemView().setVisible(true);
+//        new CustomerView().setVisible(true);
+//        new itemView().setVisible(true);
+new OrderView().setVisible(true);
     }
     
 }
